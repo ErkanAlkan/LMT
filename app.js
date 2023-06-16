1f560b58c1f666ca54ce81bdcd96514e9ab2ac7a
Erkan Alkan <erkanalkanmetu@hotmail.com>
2023-06-16 20:46:07 +0200

    app.js update

--- app.js
+++ app.js
@@ -49,13 +49,16 @@ app.get("/Todolists", function (req, res) {
   });
   List.find({}).select("name").then(function (allLists) {
     List.findOne({ name: selectedListName }).then(function (foundList) {
-      if (foundList.items.length != 0) {
-        selectedListItems = foundList.items;
-        selectedListCheckboxes = foundList.checkboxStatus;
-      } else if (foundList.items.length === 0){
+      if (!foundList) {
+        if (foundList.items.length != 0) {
+          selectedListItems = foundList.items;
+          selectedListCheckboxes = foundList.checkboxStatus;
+        } 
+      }else{
         selectedListItems = [];
         selectedListCheckboxes = [];
       }
+      
       
       res.render("list", { allLists: allLists, selectedListName: selectedListName, selectedListItems: selectedListItems, selectedListCheckboxes: selectedListCheckboxes });
     });
